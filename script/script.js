2e1cc5357d1960201880d1797ae03feac67a7f84
Julian <julian_ludwig@web.de>
2021-12-23 13:00:15 +0100

    translate text

--- script/script.js
+++ script/script.js
@@ -1,19 +1,13 @@
-let flocken = ["❄","*","+","-"];
-let counter = 0;
-let cnt = document.getElementById("cnt").innerHTML;
-
+let chars = ["❄", "*", "+", "-"];
 function letItSnow() {
-    const schneeflocke = document.createElement("div");
-    schneeflocke.innerHTML = flocken[Math.floor(Math.random()*flocken.length)];
-    schneeflocke.classList.add('schnee');
-    schneeflocke.style.left = Math.random() * 100 + "%";
-    schneeflocke.style.animationDuration = 5 + Math.random() * 15 + "s";
-    schneeflocke.style.fontSize = Math.random() * 2 + "vw";
-    schneeflocke.style.opacity = Math.random();
-    schneeflocke.style.filter = "blur(" + Math.random() * 3 + "px)";
-
-    document.body.appendChild(schneeflocke);
-    cnt = counter++;
+    const snowflake = document.createElement("div");
+    snowflake.innerHTML = chars[Math.floor(Math.random() * chars.length)];
+    snowflake.classList.add("snow");
+    snowflake.style.left = Math.random() * 100 + "%";
+    snowflake.style.animationDuration = 5 + Math.random() * 15 + "s";
+    snowflake.style.fontSize = Math.random() * 2 + "vw";
+    snowflake.style.opacity = Math.random();
+    snowflake.style.filter = "blur(" + Math.random() * 3 + "px)";
 
     setTimeout(() =>    {schneeflocke.remove();
                          cnt = counter--;}, 15000 )
